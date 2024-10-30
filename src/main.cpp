e651982b98242a635bb9366e6882f1f798b04001
织蛾 <QianMoth@qq.com>
2024-10-30 18:09:28 +0800

    添加实体按键功能 (#1)
    * 实现翻转开关就翻转输出状态
    
    * 优化灯光，连接成功10s后熄灭
    
    * 实现3s内连续多次开关，重新配网
    
    * 添加序列号和型号

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,41 @@
 #include "header.h"
 #include "HomeSpan.h"
 
+class InvertedLED : public Blinkable
+{
+    int pin;
+
+  public:
+    InvertedLED(int pin) : pin{pin}
+    {
+        pinMode(pin, OUTPUT);
+        digitalWrite(pin, HIGH);
+    }
+
+    void on() override { digitalWrite(pin, LOW); }
+    void off() override { digitalWrite(pin, HIGH); }
+    int getPin() override { return (pin); }
+};
+
 class DevLed : public Service::LightBulb
 {
     int ledPin;
+    int powerPin;
     SpanCharacteristic *power;
 
+    int resetCount{0};
+    uint32_t resetAlarm{0};
+    uint32_t resetTime{3000};
+
   public:
-    DevLed(int ledPin)
+    DevLed(int ledPin, int powerPin)
     {
         power = new Characteristic::On();
         this->ledPin = ledPin;
         pinMode(ledPin, OUTPUT);
+
+        new SpanToggle(powerPin);
+        this->powerPin = powerPin;
     }
 
     boolean update()
@@ -19,27 +43,67 @@ class DevLed : public Service::LightBulb
         digitalWrite(ledPin, power->getNewVal());
         return (true);
     }
+
+    void button(int pin, int pressType) override
+    {
+        unsigned long cTime = millis();
+
+        if (resetCount == 0 || cTime > resetAlarm) {
+            resetAlarm = cTime + resetTime;
+            resetCount = 0;
+        }
+
+        if (pressType == SpanButton::OPEN || pressType == SpanButton::CLOSED) {
+            power->setVal(1 - power->getVal());
+
+            resetCount++;
+        }
+
+        if (resetCount == 7 && cTime < resetAlarm) {
+            homeSpan.processSerialCommand("A");
+
+            digitalWrite(ledPin, LOW);
+            return;
+        }
+
+        digitalWrite(ledPin, power->getVal());
+    }
 };
+
+String getID()
+{
+#if defined(ESP8266)
+    String id(ESP.getChipId());
+#elif defined(ESP32)
+    String id((uint32_t)ESP.getEfuseMac(), HEX);
+#endif
+    id.toUpperCase();
+    return id;
+}
 
 void setup()
 {
-    pinMode(LED_PIN, OUTPUT);
-    pinMode(BUTTON_PIN, INPUT_PULLUP);
-
     Serial.begin(115200);
 
+    homeSpan.setStatusAutoOff(10);  // 10s
+    homeSpan.setStatusDevice(new InvertedLED(LED_PIN));
     homeSpan.setControlPin(BUTTON_PIN);
-    homeSpan.setStatusPin(LED_PIN);
+
+    homeSpan.setApSSID("MothHomeSetup");
+    homeSpan.setApPassword("97654321");
+
     homeSpan.begin(Category::Outlets, "Moth Switch");
 
     new SpanAccessory();
     new Service::AccessoryInformation();
     new Characteristic::Manufacturer("MOTH");
+    new Characteristic::SerialNumber(getID().c_str());
+    new Characteristic::Model("A1");
     new Characteristic::FirmwareRevision(FIRMWARE_VERSION);
     new Characteristic::HardwareRevision(HARDWARE_VERSION);
     new Characteristic::Identify();
 
-    new DevLed(IO_PIN);
+    new DevLed(IO_PIN, KEY_PIN);
 }
 
 void loop()
